fd05b59e50bfa8a1ba9f30ef0f5d7c290b2e7fdd
Patrice Gerard <patricegerardjr@gmail.com>
2017-08-16 17:00:57 -0400

    Initial Commit
    Beginning of Javascript Basics course

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,23 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+    Created by Patrice Gerard
+*/
+
+var firstname = "Patrice";
+var lastName = "Gerard";
+var fullName = firstname + " " + lastName;
+var age = 12;
+var awesomeThoughts = "I am " + firstname + " and I am awesome";
+var email = "patrice@udacity.com"
+var newEmail = email.replace("udacity", "gmail");
+var funThoughts = awesomeThoughts.replace("awesome", "fun");
+var role = "Software Engineer";
+var formattedName = HTMLheaderName.replace("%data%", fullName);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var skills = ["awesome", "programming", "teaching", "js"];
+
+
+
